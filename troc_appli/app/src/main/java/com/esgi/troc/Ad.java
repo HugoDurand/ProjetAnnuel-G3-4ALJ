58ae779232e5f5d21ebcd755b6f4c164ab512cc8
Celia <massicilia.nedir@outlook.com>
2018-06-25 10:20:57 +0200

    creation of the app

---
+++ troc_appli/app/src/main/java/com/esgi/troc/Ad.java
@@ -0,0 +1,50 @@
+package com.esgi.troc;
+
+public class Ad {
+
+    private String message;
+    private String localisation;
+    private String price;
+    private String statut;
+
+    Ad(String message, String localisation, String price, String statut)
+    {
+        this.message = message;
+        this.localisation = localisation;
+        this.price = price;
+        this.statut = statut;
+    }
+
+    public void setMessage()
+    {
+        this.message = message;
+    }
+    public void setLocalisation()
+    {
+        this.localisation = localisation;
+    }
+    public void setPrice()
+    {
+        this.price = price;
+    }
+    public void setStatut()
+    {
+        this.statut = statut;
+    }
+    public String getMessage()
+    {
+        return this.message;
+    }
+    public String getLocalisation()
+    {
+        return this.localisation;
+    }
+    public String getPrice()
+    {
+        return this.price;
+    }
+    public String getStatut()
+    {
+        return this.statut;
+    }
+}
